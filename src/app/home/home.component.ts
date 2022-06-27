e4bd98195aad4c068d892a91764d0081ccaef44d
Igor Pierre <62574338+ipcm27@users.noreply.github.com>
2022-06-26 22:22:29 -0300

    adicionando método get

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { AlticciSequenceService } from '../service/alticci-sequence.service';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
+
+import { HttpClient } from '@angular/common/http';
+import { AlticciNumber } from '../model/AlticciNumber';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  AlticciNumber!: any;
+  AlticciNumberResultObject!: AlticciNumber;
+  AlticciNumber: string = '';
+  API = 'http://localhost:8080/alticci/';
 
-  constructor(private AlticciService: AlticciSequenceService) {}
-
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {}
 
-  getNumber(input: string) {
-    this.AlticciNumber = this.AlticciService.getAlticciNumber(parseInt(input));
-    return this.AlticciNumber;
+  getAlticciNumber(n: any) {
+    parseInt(n);
+    const URL = `${this.API}${n}`;
+    this.http.get<AlticciNumber>(URL).subscribe(
+      res => {
+        this.AlticciNumberResultObject = res;
+        this.AlticciNumber = this.AlticciNumberResultObject.number.toString();
+        console.log(this.AlticciNumberResultObject);
+        return this.AlticciNumberResultObject;
+      },
+      err => {
+        console.log('aconteceu um erro:' + err);
+        return err;
+      }
+    );
   }
 }
--- src/app/service/alticci-sequence.service.ts
+++ src/app/service/alticci-sequence.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-interface ApiResponse {
-  AlticciNumber: number;
-}
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { AlticciNumber } from '../model/AlticciNumber';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AlticciSequenceService {
+  AlticciNumberResult!: AlticciNumber;
+  API = 'http://localhost:8080/alticci/';
+
   constructor(private http: HttpClient) {}
-  URL = 'http://localhost:8080/alticci/';
 
-  getAlticciNumber(n: number) {
-    let response = this.http.get<ApiResponse>(`${this.URL}${n}`);
-
-    console.log(response);
-    return response;
+  getAlticciNumber(n: any) {
+    const URL = `${this.API}${n}`;
+    this.http.get<AlticciNumber>(URL).subscribe(
+      res => {
+        this.AlticciNumberResult = res;
+        console.log(this.AlticciNumberResult);
+        return this.AlticciNumberResult;
+      },
+      err => {
+        console.log('aconteceu um erro:' + err);
+        return err;
+      }
+    );
   }
 }
